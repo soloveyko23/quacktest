6188c55ba2c53b7930bb0b394c90339253dc0e38
Maksym Soloviov <42580651+soloveyko23@users.noreply.github.com>
2024-06-13 15:39:16 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -9,5 +9,26 @@
             }));
         }
     }), 0);
+    document.addEventListener("DOMContentLoaded", (() => {
+        const menuItems = document.querySelectorAll(".main-menu__item");
+        if (menuItems.length === 0) {
+            console.error("No elements with class 'main-menu__item' found.");
+            return;
+        }
+        menuItems.forEach((item => {
+            item.addEventListener("click", (() => {
+                menuItems.forEach((el => {
+                    if (el !== item) el.classList.add("disabled");
+                }));
+                item.classList.add("animate");
+                setTimeout((() => {
+                    item.classList.remove("animate");
+                    menuItems.forEach((el => {
+                        el.classList.remove("disabled");
+                    }));
+                }), 500);
+            }));
+        }));
+    }));
     window["FLS"] = true;
 })();
